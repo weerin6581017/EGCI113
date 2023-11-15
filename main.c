09c9f66977d0830cb32416741b3edb12e351aa69
weerin6581017 <146055310+weerin6581017@users.noreply.github.com>
2023-11-15 11:03:23 +0700

    assignment10(string)

--- main.c
+++ main.c
@@ -1,17 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
 int main()
 {
-    int n,i,num;
-    printf("Input a number:");
-    scanf("%d",&n);
-    printf("The reverse of the number is ");
-    for(i=n;i>0;i/=10)
-    {
-        num=i%10;
-        printf("%d",num);
+    int i;
+    char str[3][30];
+
+
+    //input
+    for(i=0;i<3;i++){
+    printf("Input string %d:",i+1);
+    scanf("%s",str[i]);
+  }
+
+    //print
+    for(i=0;i<3;i++){
+    printf("%s\n",str[i]);
     }
-    printf(".");
+ char first[30],last[30];
+    //compare firstlast
+   strcpy(first,str[0]);
+    for(i=0;i<3;i++){
+            if(strcmp(first,str[i])>0) strcpy(first,str[i]);
+    }
+   strcpy(last,str[0]);
+     for(i=0;i<3;i++){
+              if(strcmp(last,str[i])<0) strcpy(last,str[i]);
+    }
+
+    printf("first string = %s\nlast string = %s\n",first,last);
+
+    //concaternate first+last
+    printf("%s\n",strcat(first,last));
+
+
+    //print reverse
+    for(i=strlen(first)-1;i>=0;i--){
+            switch(first[i]){
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u': printf("%c",toupper(first[i]));
+        break;
+        default: printf("%c",first[i]);
+            }
+
+    }
+
     return 0;
 }
